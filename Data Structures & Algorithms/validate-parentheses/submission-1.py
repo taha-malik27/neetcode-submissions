5066f6f32953c29b6588d5d7883b18e93e23e189
Taha Malik <tahammalikbo3@gmail.com>
2026-09-08 15:32:23 -0600

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        stack = [] # use to track opening parenthesis
+        pairs = ["[]" ,"()" , "{}"]  # used to check if closing parenthesis makes a valid pair
+        for p in s:
+            # add opening parenthesis
+            if p in ["[", "{", "("]:
+                stack.append(p)
+                continue
+
+            # for closing parenthesis, first check if stack is empty
+            if stack:
+                # if not empty, now check if last item in stack is matching opening parenthesis
+                if stack[-1] + p in pairs:
+                    # if pair is good, pop opener and move to next iteration
+                    stack.pop()
+                else:
+                    # if no good pair, return False
+                    return False
+            else:
+                # if empty, theres an extra closing, so return False
+                return False
+        
+        # stack must be empty otherwise it implies extra openers
+        return not stack
+            
+        
+        
